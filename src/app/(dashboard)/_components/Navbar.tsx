dc90ade615d0c6e88d4e15fd5f2132d8579c110b
Itay Sidis <itaysidis@gmail.com>
2023-10-25 18:00:01 +0300

    Mobile sidebar. installed shadcn ui sheet

---
+++ src/app/(dashboard)/_components/Navbar.tsx
@@ -0,0 +1,13 @@
+import NavbarRoutes from "@/components/NavbarRoutes";
+import MobileSidebar from "./MobileSidebar";
+
+export const Navbar = () => {
+  return (
+    <nav className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+      <MobileSidebar />
+      <NavbarRoutes />
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/app/(dashboard)/_components/Sidebar.tsx
+++ src/app/(dashboard)/_components/Sidebar.tsx
@@ -3,9 +3,9 @@ import SidebarRoutes from "./SidebarRoutes";
 
 export const Sidebar = () => {
   return (
-    <aside className="sidebar hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50 border-r overflow-y-auto bg-white shadow-sm">
+    <>
       <Logo />
       <SidebarRoutes />
-    </aside>
+    </>
   );
 };
